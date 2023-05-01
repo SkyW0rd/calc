bb15a9177385fe044ed2e76fd5b5e041274908ac
SkyW0rd <101702769+SkyW0rd@users.noreply.github.com>
2023-05-01 13:31:00 +0300

    Add files via upload

--- calc.h
+++ calc.h
@@ -4,57 +4,93 @@
 template<typename T, typename Temp>
 void _add(T& x, Temp y)
 {
-
+	x += y;
 }
 template<typename T, typename Temp>
 void _sub(T& x, Temp y)
 {
-
+	x -= y;
 }
 template<typename T, typename Temp>
 void _mul(T& x, Temp y)
 {
-
+	x *= y;
 }
 template<typename T, typename Temp>
 void _div(T& x, Temp y)
 {
-
-}
-template<typename T>
-void _pow(T& a, int b)
-{
-	T x = a;
-	if (b > 1)
-	{
-		for (int i = 1; i < b; i++)
-		{
-			a = a * x;
-		}
-	}
-	else if (b == 0)
-	{
-		a = 1;
-	}
+	x /= y;
 }
 template<typename T>
 void _sqrt(T& x, int n)
 {
-	T  t, m = x / 2;
-	do {
-		t = m;
-		m = (t + (x / t)) / n;
-	} while ((t - m) != 0);
-	x = m;
+	float eps = 0.00001;
+	float root = x / n;
+	float rn = x;
+	while (abs(root - rn) >= eps)
+	{
+		rn = x;
+		for (int i = 1; i < n; i++)
+		{
+			rn /= root;
+		}
+		root = 0.5 * (rn + root);
+	}
+	x = root;
+}
+template<typename T>
+void _pow(T& a, float b)
+{
+	T x = a;
+	int re = b;
+	int im = 100 / (float(abs(b - re)) * 100);
+	if (b == 0)
+	{
+		a = 1;
+	}
+	else if (b > 0)
+	{
+		if (b != 1)
+		{
+			_sqrt(a, im);
+			for (int i = 0; i < re; i++)
+			{
+				a *= x;
+			}
+		}
+	}
+	else if (b < 0)
+	{
+		if (b != -1)
+		{
+			_sqrt(a, im);
+			for (int i = 0; i < abs(re); i++)
+			{
+				a *= x;
+			}
+			a = 1 / a;
+		}
+		else
+		{
+			a = 1 / a;
+		}
+	}
 }
 template<class T>
 void _ln(T& x)
 {
-}
-template<class T>
-void _lg(T& x, T b)
-{
-
+	T a = x;
+	T powa = x;
+	T sum = 0;
+	int sig = 1;
+	int n = 0;
+	while (abs(a) > 0)
+	{
+		sum += a;
+		sig = -sig;
+		a = sig * (powa *= x) / ++n;
+	}
+	x = sum;
 }
 template<class T>
 void _sin(T& x)
--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include"calc.h"
 int main()
 {
-	int a = 1000;
-	_sqrt(a, 2);
+	double a = 1000;
+	_ln(a);
 	std::cout << a << std::endl;
-	_pow(a, 2);
-	std::cout << a << std::endl;
-	testing::InitGoogleTest();
-	RUN_ALL_TESTS();
+	//testing::InitGoogleTest();
+	//RUN_ALL_TESTS();
 	return 0;
 }
